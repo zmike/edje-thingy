afba84acf69755f6cec99f9f2da5518a4485ea90
discomfitor <mike@zentific.com>
2011-01-22 16:06:02 -0500

    only rebuild lemon when it doesn't exist

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CFLAGS="-O0 -pipe -Wall -Wextra -g -ggdb3 $(pkg-config --cflags --libs ecore) -DHAVE_ALLOCA_H -D_GNU_SOURCE"
 
-gcc -o lemon lemon.c -g
+[[ -f lemon ]] || gcc -o lemon lemon.c -g
 
 re2c -bi -o edje_parser.y edje_parser.yre
 ./lemon -q edje_parser.y
