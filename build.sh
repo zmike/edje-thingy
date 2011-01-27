5bb8a9bb188abceb7db21c40c2083276bb064f92
discomfitor <mike@zentific.com>
2011-01-27 13:30:16 -0500

    only use re2c when needed

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ CFLAGS="-O0 -pipe -Wall -Wextra -g -ggdb3 $(pkg-config --cflags --libs ecore-fil
 [[ -f lemon ]] || gcc -o lemon lemon.c -g
 [[ -n $CC ]] || CC=gcc
 
-re2c -bi -o edje_parser.y edje_parser.yre
+[[ -f edje_parser.y ]] || re2c -bi -o edje_parser.y edje_parser.yre
 ./lemon -q edje_parser.y
 $CC $CFLAGS -I. -o edje_parser edje*.c
