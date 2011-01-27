a1beb8dc20c18f1478f5a352fa5712c7c15e0260
discomfitor <mike@zentific.com>
2011-01-27 12:45:05 -0500

    allow use of other CC with $CC set

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 CFLAGS="-O0 -pipe -Wall -Wextra -g -ggdb3 $(pkg-config --cflags --libs ecore-file) -DHAVE_ALLOCA_H -D_GNU_SOURCE -lm"
 
 [[ -f lemon ]] || gcc -o lemon lemon.c -g
+[[ -n $CC ]] || CC=gcc
 
 re2c -bi -o edje_parser.y edje_parser.yre
 ./lemon -q edje_parser.y
-gcc $CFLAGS -I. -o edje_parser edje*.c
+$CC $CFLAGS -I. -o edje_parser edje*.c
